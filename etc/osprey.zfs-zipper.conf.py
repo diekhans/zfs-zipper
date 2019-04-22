e85ba58492b985f9ae3ac3a9b98df1cd95396756
Mark Diekhans <markd@kermodei.com>
2019-04-21 19:10:34 -0700

    split osprey to two packup sets

--- etc/osprey.zfs-zipper.conf.py
+++ etc/osprey.zfs-zipper.conf.py
@@ -3,18 +3,20 @@ from zfszipper.config import BackupSetConf, SourceFileSystemConf, BackupPoolConf
 
 
 # backup pool disks are zfs-mirrored and compressed
-#   zpool create -O atime=off -O compression=lz4 -O dedup=on -m /media/zackup1 osprey_zackup1a ada0 ada1
+#   zpool create -O atime=off -O compression=lz4 -O dedup=off -m /media/zackup1 osprey_zackup1a mirror /dev/disk0 /dev/disk1
 
-# three rotations
+# two rotations
 osprey1Set = BackupSetConf("osprey1",
+                           [SourceFileSystemConf("b_pool/photo_b")],
+                           [BackupPoolConf("osprey_zackup1a"),
+                            BackupPoolConf("osprey_zackup1b")])
+osprey2Set = BackupSetConf("osprey2",
                            [SourceFileSystemConf("a_pool/markd_a"),
                             SourceFileSystemConf("a_pool/osprey"),
-                            SourceFileSystemConf("a_pool/photo_a"),
-                            SourceFileSystemConf("b_pool/photo_b")],
-                           [BackupPoolConf("osprey_zackup1a"),
-                            BackupPoolConf("osprey_zackup1b"),
-                            BackupPoolConf("osprey_zackup1c")])
-config = BackupConf([osprey1Set],
+                            SourceFileSystemConf("a_pool/photo_a")],
+                           [BackupPoolConf("osprey_zackup2a"),
+                            BackupPoolConf("osprey_zackup2b")])
+config = BackupConf([osprey1Set, osprey2Set],
                     lockFile="/var/run/zfszipper.lock",
                     recordFilePattern="/var/db/zfszipper/%Y/%Y-%m.record.tsv",
                     syslogFacility="local0")
