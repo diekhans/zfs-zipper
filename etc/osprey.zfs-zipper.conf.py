11210f6fad2a25438fe1db06ce3881e2cdde591f
Mark Diekhans <markd@kermodei.com>
2019-05-21 21:31:56 -0700

    added backup rotation

--- etc/osprey.zfs-zipper.conf.py
+++ etc/osprey.zfs-zipper.conf.py
@@ -5,17 +5,23 @@ from zfszipper.config import BackupSetConf, SourceFileSystemConf, BackupPoolConf
 # backup pool disks are zfs-mirrored and compressed
 #   zpool create -O atime=off -O compression=lz4 -O dedup=off -m /media/zackup1 osprey_zackup1a mirror /dev/disk0 /dev/disk1
 
+# pools are names in the form osprey_zackup{set}{rotation}
+# where set corresponds to a backupset and rotation is a rotation of that set,
+# (a, b, c)
+
 # two rotations
 osprey1Set = BackupSetConf("osprey1",
                            [SourceFileSystemConf("b_pool/photo_b")],
                            [BackupPoolConf("osprey_zackup1a"),
-                            BackupPoolConf("osprey_zackup1b")])
+                            BackupPoolConf("osprey_zackup1b"),
+                            BackupPoolConf("osprey_zackup1c")])
 osprey2Set = BackupSetConf("osprey2",
                            [SourceFileSystemConf("a_pool/markd_a"),
                             SourceFileSystemConf("a_pool/osprey"),
                             SourceFileSystemConf("a_pool/photo_a")],
                            [BackupPoolConf("osprey_zackup2a"),
-                            BackupPoolConf("osprey_zackup2b")])
+                            BackupPoolConf("osprey_zackup2b"),
+                            BackupPoolConf("osprey_zackup2c")])
 config = BackupConf([osprey1Set, osprey2Set],
                     lockFile="/var/run/zfszipper.lock",
                     recordFilePattern="/var/db/zfszipper/%Y/%Y-%m.record.tsv",
