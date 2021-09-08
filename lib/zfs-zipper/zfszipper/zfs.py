9de892e32fb69ef7705c6afb8d6cddc81ff785f4
Mark Diekhans <markd@ucsc.edu>
2021-09-07 17:53:04 -0700

    disable restartable send, becoming a headache until added to this code

--- lib/zfs-zipper/zfszipper/zfs.py
+++ lib/zfs-zipper/zfszipper/zfs.py
@@ -106,7 +106,7 @@ class Zfs(object):
         "return results of send -P parsed into rows of columns"
         # receive -F is require to prevent "destination X has been modified" error
         sendCmd = ["zfs", "send", "-P", "-i", sourceBaseSnapshotName, sourceSnapshotName]
-        recvCmd = ["zfs", "receive", "-s", "-F", backupSnapshotName]
+        recvCmd = ["zfs", "receive", "-F", backupSnapshotName]
         stderr1, ignored = self.cmdRunner.pipeline2(sendCmd, recvCmd)
         return splitTabLinesToRows(stderr1)
 
